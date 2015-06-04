d7a452f55207fd546e81e50d6eb83cb0fdcdb7b6
Edwin Acevedo <edwin.a.acevedo@gmail.com>
2015-06-04 15:14:16 -0500

    adds function to add object to array

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,55 @@
-var app = angular.module('angularAddresses', []);
-
-app.controller('Main', function () {
+var app = angular
+  .module('angularAddresses', [])
+  .filter('objToArr', function () {
+    return function (obj) {
+      if (obj) {
+        return Object
+          .keys(obj)
+          .map(function (key) {
+            obj[key]._id = key;
+            return obj[key];
+        console.log(key); //does not see this
+        console.log(obj); //also does not see this
+        });
+      }
+    };
+  })
+  .filter('ransomcase', function () {
+    return function (string) {
+      return string
+        .split('')
+        .map(function (char, i) {
+          return i % 2 ? char.toUpperCase() : char.toLowerCase();
+        })
+        .join('');
+    };
+  });
+app.controller('Main', function ($http) {
   var vm = this; //so that the app can grow and this can be used within local scope
-  vm.people = [
-    {
-      name: 'John',
-      phone: '(111) 111-1111',
-      twitter: '@john',
-      photo: 'http://i.imgur.com/P7pkOEF.jpg'
-
-    },
-    {
-      name: 'Jennifer',
-      phone: '(222) 222-2222',
-      twitter: '@jenster',
-      photo: 'http://i.imgur.com/P7pkOEF.jpg'
-
-    },
-    {
-      name: 'James',
-      phone: '(333) 333-3333',
-      twitter: '@jimbo',
-      photo: 'http://i.imgur.com/P7pkOEF.jpg'
-
-    },
-    {
-      name: 'Chelsea',
-      phone: '(444) 444-4444',
-      twitter: '@chels',
-      photo: 'http://i.imgur.com/P7pkOEF.jpg'
-
-    },
-    {
-      name: 'Edwin',
-      phone: '(555) 555-5555',
-      twitter: '@edstros',
-      photo: 'http://i.imgur.com/P7pkOEF.jpg'
-
-    },
-  ];
+  $http
+    .get('https://addressangularity.firebaseio.com/people.json')
+    .success(function (data) {
+      vm.people = data;
+    console.log(data); //sees the whole firebase
+    });
   vm.newPerson = {};
   vm.addNewContact = function () { //reference on the button in the html
-    vm.people.push(vm.newPerson); //creates the new person in the table
-    vm.newPerson = {}; // clears out the form
+    $http
+      .post('https://addressangularity.firebaseio.com/people.json', vm.newPerson)
+      .success(function () {
+        vm.people.push(vm.newPerson); //creates the new person in the table
+        vm.newPerson = {}; // clears out the form
+        $('#modal').modal('hide');
+      });
   };
-  vm.removeContact = function (person) {
-    console.log(person);
-    console.log(vm.people);
-    var index = vm.people.indexOf(person);
-    vm.people.splice(index, 1);
+  vm.removeContact = function (id) {
+    $http
+      .delete('https://addressangularity.firebaseio.com/' + id + '.json')
+      .success(function () {
+        delete vm.people[id];
+        console.log(id);//never sees this
+        //var index = vm.people.indexOf(person);
+          //vm.people.splice(index, 1);
+      });
   };
-
 });
